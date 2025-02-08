a4e81184a6a6aaeb526346bc409164217330c65d
von-development <vondevelopment@gmail.com>
2025-02-08 19:08:37 +0000

    Erro no mapaaaaa

--- components/markdown/MarkdownListItem.tsx
+++
@@ -1,7 +0,0 @@
-interface MarkdownListItemProps {
-  children: React.ReactNode;
-}
-
-export const MarkdownListItem = ({ children }: MarkdownListItemProps) => {
-  return <li className="pl-1 list-disc">{children}</li>;
-}; 
--- components/markdown/MarkdownParagraph.tsx
+++
@@ -1,7 +0,0 @@
-interface MarkdownParagraphProps {
-  children: React.ReactNode;
-}
-
-export const MarkdownParagraph = ({ children }: MarkdownParagraphProps) => {
-  return <p className="my-4 break-words">{children}</p>;
-}; 
