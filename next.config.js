6118e1f8f0f72cb1e90fe2aac9439c7f32bcf71a
von-development <vondevelopment@gmail.com>
2025-02-13 12:03:18 +0000

    Fix: Update dependencies and build configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... your other config
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+      { module: /node_modules\/punycode\/punycode\.js/ },
+    ];
+    return config;
+  },
+}
+
+module.exports = nextConfig 
